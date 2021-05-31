db29998a66b8088c6b0009264fa9f6cd3d1e6633
faizan <faizan.meo179@gmail.com>
2021-05-31 18:03:21 +0500

    Navbar Completed

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import {AppBar, Toolbar, Typography} from "@material-ui/core";
+import SportsCricketIcon from '@material-ui/icons/SportsCricket';
+import "../Styles/NavbarStyles.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <>
+                <AppBar position="fiexd">
+                    <Toolbar>  
+                        <SportsCricketIcon className="circketIcon"/>           
+                        <Typography variant="h5" mb={3}>Cricket Live Score</Typography>
+                    </Toolbar>
+                </AppBar>
+                
+            </>
+        )
+    }
+}
+
+export default Navbar
