38b79bffaccdbde4de41b0de693af03a4ef4f6cb
faizan <faizan.meo179@gmail.com>
2021-06-04 20:23:00 +0500

    Score component is completed

--- src/Component/Score.jsx
+++ src/Component/Score.jsx
@@ -18,7 +18,7 @@ const styles = {
         padding: "8px",
     },
     showScore:{
-        display: "flex",
+        display: "block",
     }
 }
 
@@ -29,6 +29,13 @@ class Score extends Component {
             details : {}
         }
     }
+    // componentDidMount(){
+    //     getCricketMatchesScore(this.props.match.unique_id).then(data =>{
+    //         this.setState({details: this.state.details = data});
+    //         console.log("Inside getCricketMatchesScore")
+    //         console.log(data)
+    //     }).catch(error => console.log(error));
+    // }
     getScore(id){
         getCricketMatchesScore(id).then(data =>{
             this.setState({details: this.state.details = data});
@@ -36,18 +43,18 @@ class Score extends Component {
             console.log(data)
         }).catch(error => console.log(error));
     }
-    // getScoreString = (score) =>{
-    //     console.log("Happy")
-    //     if(!score) return {}
-    //     let num = score && score.split("/");
-    //     let numid = num && num[1].split("v");
-    //     let ans = {};
-    //     ans.first = num[0] && numid[0]  && num[0].replace(/[^0-9]/g, "") + "/" + numid[0].replace(/[^0-9]/g, "");
-    //     ans.second = num[2] && numid[1]  && numid[1].replace(/[^0-9]/g, "") + "/" + numid[2].replace(/[^0-9]/g, "");
-    //     return ans;
-    // }
+    getScoreString = (score) =>{
+        console.log("Happy")
+        if(!score) return {}
+        let num = score && score.split("/");
+        let numid = num && num[1].split("v");
+        let ans = {};
+        ans.first = num[0] && numid[0]  && num[0].replace(/[^0-9]/g, "") + "/" + numid[0].replace(/[^0-9]/g, "");
+        ans.second = num[2] && numid[1]  && numid[1].replace(/[^0-9]/g, "") + "/" + num[2].replace(/[^0-9]/g, "");
+        return ans;
+    }
     render() {
-        // let score = this.getScoreString(this.state.details.score);
+        let score = this.getScoreString(this.state.details.score);
 
         return (
             <div className="score_wraper">
@@ -55,7 +62,7 @@ class Score extends Component {
                 <Card elevation={10} className="card_wraper">
                         <Box className={this.props.classes.scoreCardHeader}>
                             <Typography>{this.props.match["team-1"]} VS {this.props.match["team-2"]}</Typography>
-                            <Button color="secondary" size="small" style={{marginLeft: "auto", color: "#fff", border: "2px solid #fff"}} onClick={()=> this.getScore(this.props.match.unique_id)} disabled={this.props.match.matchStarted ? false : true}>Get Score</Button>
+                            <Button  variant="contained" color="secondary" size="small" style={{marginLeft: "auto"}} onClick={()=> this.getScore(this.props.match.unique_id)} disabled={this.props.match.matchStarted ? false : true}>Get Score</Button>
                         </Box>
                         <CardContent>
                             <Box className={this.props.classes.timeDate}>
@@ -66,21 +73,27 @@ class Score extends Component {
                                 <Box className="card_content">
                                     <Grid>
                                         <Typography variant="h5" style={{margin: "32px"}}>{this.props.match["team-1"]}</Typography>
-                                        {/* <Typography>{score.first}</Typography> */}
+                                        <Typography>{score.first}</Typography>
                                     </Grid>
                                      <Grid>
                                         <img src={vs} alt="verses" width={100} height={100} />
                                      </Grid>
                                      <Grid>
                                         <Typography variant="h5" style={{margin: "32px"}}>{this.props.match["team-2"]}</Typography>
-                                        {/* <Typography>{score.second}</Typography> */}
+                                        <Typography>{score.second}</Typography>
                                      </Grid>
                                 </Box>
                             </Grid>
                         </CardContent>
                         <CardActions>
                             <Grid container justify="center" className={this.props.classes.showScore} >
-                                <Typography>{this.state.details.score}</Typography>
+                            {/* {
+                                this.state.details.score 
+                                ?<Typography>{this.state.details.score}</Typography>
+                                :"Not"
+                            }  */}
+
+                                <Typography variant="body2">{this.state.details.score}</Typography>
                                 <Typography variant="body2">Limited in Person attendance</Typography>
                             </Grid>
                         </CardActions>
